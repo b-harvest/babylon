d0b9a4123dde216addeb5e728e16b455f6eda1ad
wonjoon <wnjoon@gmail.com>
2025-01-17 19:36:47 +0900

    feat: add error msg when InitPrivSigner is called without key files
    (cherry picked from commit 280de7e65f206d04a763d5de0a956984235a4e7e)

--- app/signer/private.go
+++ app/signer/private.go
@@ -6,6 +6,7 @@ import (
 	cmtconfig "github.com/cometbft/cometbft/config"
 
 	"github.com/babylonlabs-io/babylon/privval"
+	cmtos "github.com/cometbft/cometbft/libs/os"
 	cmtprivval "github.com/cometbft/cometbft/privval"
 )
 
@@ -24,6 +25,14 @@ func InitPrivSigner(nodeDir string) (*PrivSigner, error) {
 
 	if err := privval.EnsureDirs(pvKeyFile, pvStateFile, blsKeyFile, blsPasswordFile); err != nil {
 		return nil, fmt.Errorf("failed to ensure dirs: %w", err)
+	}
+
+	if !cmtos.FileExists(pvKeyFile) {
+		return nil, fmt.Errorf("validator key file does not exist. create file using `babylond init`: %s", pvKeyFile)
+	}
+
+	if !cmtos.FileExists(blsKeyFile) || !cmtos.FileExists(blsPasswordFile) {
+		return nil, fmt.Errorf("BLS key file does not exist. create file using `babylond init` or `babylond create-bls-key`: %s", blsKeyFile)
 	}
 
 	cometPV := cmtprivval.LoadFilePV(pvKeyFile, pvStateFile)
