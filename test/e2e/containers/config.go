9451cc8a0a49c18007667086f6281a9336bf2d53
wonjoon <wnjoon@gmail.com>
2025-01-21 19:53:24 +0900

    chore: change currentTag

--- test/e2e/containers/config.go
+++ test/e2e/containers/config.go
@@ -18,7 +18,8 @@ const (
 	BabylonContainerName = "babylonlabs-io/babylond"
 	// name of babylon image before the upgrade
 	BabylonContainerNameBeforeUpgrade = "babylonlabs/babylond"
-	BabylonContainerTagBeforeUpgrade  = "v0.9.3"
+	// BabylonContainerTagBeforeUpgrade  = "v0.9.3"
+	BabylonContainerTagBeforeUpgrade = "latest"
 
 	// name of the image produced by running `make e2e-init-chain` in contrib/images
 	InitChainContainerE2E = "babylonlabs-io/babylond-e2e-init-chain"
